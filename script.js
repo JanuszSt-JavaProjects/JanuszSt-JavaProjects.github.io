4b7c47f998bb69a6baf6d2fd73a462848729cb48
JanuszSt-JavaProjects <38794945+JanuszSt-JavaProjects@users.noreply.github.com>
2021-11-29 19:21:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -108,11 +108,11 @@ $(document).ready(function() {
    function handleTaskDeleteRequest() {
       var parentEl = $(this).parents('[data-task-id]');
       var taskId = parentEl.attr('data-task-id');
-      var requestUrl = apiRoot + 'tasks';
+      var requestUrl = apiRoot + 'tasks/'+taskId;
 
       $.ajax({
-         url: requestUrl + '/' + taskId,
-         method: 'DELETE',
+         url: requestUrl,
+         method: "DELETE",
          success: function() {
             parentEl.slideUp(400, function() { parentEl.remove(); });
          }
